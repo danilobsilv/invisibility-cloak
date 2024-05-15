98842f406fd616874e95535147bf5226a3c2c1f4
Danilo Bruno da Silva <96261259+danilobsilv@users.noreply.github.com>
2024-05-14 22:35:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from invisibility_cloak import invisible_man_effect
 
 if __name__ == "__main__":
-    video_path = "video.mp4"
+    video_path = "video_path_input_here.mp4"
 
     invisible_man_effect(video_path)
