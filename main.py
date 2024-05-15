a1d8483907049ef79d0d55417c0e52c82fcba840
Danilo Bruno da Silva <96261259+danilobsilv@users.noreply.github.com>
2024-05-14 22:33:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from invisibility_cloak import invisible_man_effect
 if __name__ == "__main__":
     video_path = "video.mp4"
 
-    # Chama a função para aplicar o efeito "homem invisível"
     invisible_man_effect(video_path)
