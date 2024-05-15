4938844cb488e21dfa9f44c180c757621453bb37
kirk <ksahdo10@gmail.com>
2024-05-15 18:28:34 -0400

    update: change code comments

--- invisibility_cloak.py
+++ invisibility_cloak.py
@@ -6,24 +6,23 @@ b, g, r = 0, 0, 0
 
 def capture_background(cap, num_frames=60):
     # Captura o fundo para um número especificado de quadros
-    for i in range(num_frames):
-        ret, background = cap.read()
+    for _ in range(num_frames):
+        _, background = cap.read()
     return background
 
 def mouseRGB(event,x,y,flags,param):
-    if event == cv2.EVENT_LBUTTONDOWN: #checks mouse left button down condition
+    if event == cv2.EVENT_LBUTTONDOWN:
         global b, g, r
         b = global_frame[y,x,0]
         g = global_frame[y,x,1]
         r = global_frame[y,x,2]
 
 
-def main():
+def invisibility_cloak():
     cv2.namedWindow('cloack')
     cv2.setMouseCallback('cloack', mouseRGB)
     cap = cv2.VideoCapture(0)
     
-    # Aguardando para capturar o fundo
     print("Posicione a câmera para capturar o fundo. Aguarde...")
     background = capture_background(cap)
     print("Fundo capturado.")
@@ -34,18 +33,15 @@ def main():
         ret, frame = cap.read()
         if not ret:
             break
-
-        # Converte o frame de BGR para HSV
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         
-        # Define o intervalo da cor a ser escondida
+        # Intervalo de Cores
         lower_bound = np.array([b - 60, g - 60, r - 60])
         upper_bound = np.array([b + 60, g + 60, r + 60])
         
-        # Cria uma máscara para a cor especificada
+        # Mascára
         mask = cv2.inRange(frame, lower_bound, upper_bound)
-        
-        # Inverte a máscara
+
+        # Inversão da Máscara
         mask_inv = cv2.bitwise_not(mask)
         
         # Extrai a parte do frame sem a cor a ser escondida
@@ -60,14 +56,9 @@ def main():
         global global_frame
         global_frame = result
 
-        # Exibe o resultado
         cv2.imshow('cloack', result)
-        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from invisibility_cloak import invisibility_cloak
 
 if __name__ == "__main__":
-    video_path = "video_path_input_here.mp4"
-
-    invisibility_cloak(video_path)
+    invisibility_cloak()
